0240c976ad64b678bc1c5edfad0dc7b216a8dc6d
Daniel Topal <dtopal@gmail.com>
2017-02-04 08:30:36 +0900

    Remove hardcoding of 2016 in ScheduleBuilder.java

--- src/reportGenerator/ScheduleBuilder.java
+++ src/reportGenerator/ScheduleBuilder.java
@@ -75,8 +75,8 @@ public class ScheduleBuilder {
 	public void buildMonth(int month) throws IOException {
 		LocalDate time = LocalDate.now().withMonth(month);
 		
-		//REMOVE THIS LATER!!!!!!!!
-		time = time.withYear(2016);
+		//USE ONLY IF NEEDING TO FORCE THE YEAR FOR A REPORT
+		//time = time.withYear(2016);
 		
 		String filename = this.tm.makeMonthlyTemplate(time);
 		
